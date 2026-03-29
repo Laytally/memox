5d3bdf927835a807cdf08c108134349a2e1b31bd
Enfia <enfiaxd@gmail.com>
2026-03-30 04:56:27 +0900

    202603300456

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ class notepad:
 	__thisTextArea = Text(__root)
 	__thisMenuBar = Menu(__root)
 	__thisFileMenu = Menu(__thisMenuBar, tearoff=0)
-	__thisEditMenu = Menu(__thisMenuBar, tearoff=0)
 	__thisHelpMenu = Menu(__thisMenuBar, tearoff=0)
 	
 	# 스크롤 설정
@@ -23,139 +22,113 @@ class notepad:
 	__file = None
 
 	def __init__(self,**kwargs):
-		
 		# 아이콘 설정
 		try:
-				self.__root.wm_iconbitmap("Notepad.ico")
+			self.__root.wm_iconbitmap("MemoX.ico")
 		except:
-				pass
+			print("아이콘 에러")
 		
 		# 창 크기 설정
 		try:
 			self.__thisWidth = kwargs['width']
 		except KeyError:
-			pass
+			print("가로창 크기 에러")
 		try:
 			self.__thisHeight = kwargs['height']
 		except KeyError:
-			pass
+			print("세로창 크기 에러")
 
 		# 창 이름 설정
-		self.__root.title("제목없음-메모장")
-		# Center the window
+		self.__root.title("제목없음 - MemoX")
+		
+		# 가운데로 창 설정
 		screenWidth = self.__root.winfo_screenwidth()
 		screenHeight = self.__root.winfo_screenheight()
-		# For left-alling
+		
+		# 왼쪽 오른쪽 정렬
 		left = (screenWidth / 2) - (self.__thisWidth / 2)
-		# For right-allign
+		
+		# 위 아래 정렬
 		top = (screenHeight / 2) - (self.__thisHeight /2)
-		# For top and bottom
 
-		self.__root.geometry('%dx%d+%d+%d' % (self.__thisWidth,
-		 									self.__thisHeight,
-											left, top))
-		# To make the textarea auto resizable
+		self.__root.geometry('%dx%d+%d+%d' % (self.__thisWidth, self.__thisHeight, left, top))
+		
+		# 텍스트 영역의 크기를 자동으로 조절
 		self.__root.grid_rowconfigure(0, weight=3)
 		self.__root.grid_columnconfigure(0, weight=3)
+		self.__thisTextArea.grid(sticky=N + E + S + W)
 
-		# Add controls (widget)
-		self.__thisTextArea.grid(sticky = N + E + S + W)
-		# To open new file
-		self.__thisFileMenu.add_command(label="New",
-										command=self.__newFile)
-		# To open a already existing file
-		self.__thisFileMenu.add_command(label="Open",
-										command=self.__openFile)
-		# To save current file
-		self.__thisFileMenu.add_command(label="Save",
-										command=self.__saveFile)
-		# To create a line in the dialog
+		# 파일 설정창
+		self.__thisMenuBar.add_cascade(label="파일", menu=self.__thisFileMenu)
+
+		# 새로운 파일 열기
+		self.__thisFileMenu.add_command(label="새로 만들기", command=self.__newFile)
+		
+		# 이미 존재하는 파일 열기
+		self.__thisFileMenu.add_command(label="열기", command=self.__openFile)
+		
+		# 파일 저장하기
+		self.__thisFileMenu.add_command(label="저장하기", command=self.__saveFile)
+		
+		# 줄 추가
 		self.__thisFileMenu.add_separator()
-		self.__thisFileMenu.add_command(label="Exit",
-										command=self.__quitApplication)
-		self.__thisMenuBar.add_cascade(label="File",
-									menu=self.__thisFileMenu)
-		# To give a feature of cut
-		self.__thisEditMenu.add_command(label="Cut",
-										command=self.__cut)
-		# to give a feature of copy
-		self.__thisEditMenu.add_command(label="Copy",
-										command=self.__copy)
-		# To give a feature of paste
-		self.__thisEditMenu.add_command(label="Paste",
-										command=self.__paste)
-		# To give a feature of editing
-		self.__thisMenuBar.add_cascade(label="Edit",
-									menu=self.__thisEditMenu)
-		# To create a feature of description of the notepad
-		self.__thisHelpMenu.add_command(label="View Help",
-										command=self.__showAbout)
-		self.__thisMenuBar.add_cascade(label="Help",
-									menu=self.__thisHelpMenu)
+		self.__thisFileMenu.add_command(label="끝내기", command=self.__quitApplication)
+
+		# 도움창
+		self.__thisMenuBar.add_cascade(label="도움말", menu=self.__thisHelpMenu)
+		self.__thisHelpMenu.add_command(label="도움말 보기", command=self.__showAbout)
 		self.__root.config(menu=self.__thisMenuBar)
 		self.__thisScrollBar.pack(side=RIGHT,fill=Y)
-		# Scrollbar will adjust automatically according to the content
+		
+		# 스크롤바 자동으로 설정
 		self.__thisScrollBar.config(command=self.__thisTextArea.yview)
 		self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
+
+	# 메모장 끝내는 함수
 	def __quitApplication(self):
 		self.__root.destroy()
-		# exit()
+
+	# 메모장 도움말 보여주는 함수
 	def __showAbout(self):
-		showinfo("Notepad",view =webbrowser.open("https://www.bing.com/search?q=get+help+with+notepad+in+windows+10&filters=guid:%224466414-en-dia%22%20lang:%22en%22&form=T00032&ocid=HelpPane-BingIA"))
+		showinfo("Notepad",view =webbrowser.open("https://github.com/Laytally/memox"))
+	
+	# 메모장 여는 함수
 	def __openFile(self):
-		self.__file = askopenfilename(defaultextension=".txt",
-									filetypes=[("All Files","*.*"),
-										("Text Documents","*.txt")])
+		self.__file = askopenfilename(defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt")])
+		
+		# 조건문
 		if self.__file == "":
-			# no file to open
 			self.__file = None
 		else:
-			# Try to open the file
-			# set the window title
-			self.__root.title(os.path.basename(self.__file) + " - Notepad")
+			self.__root.title(os.path.basename(self.__file) + " - MemoX")
 			self.__thisTextArea.delete(1.0,END)
 			file = open(self.__file,"r")
 			self.__thisTextArea.insert(1.0,file.read())
 			file.close()
 
+	# 메모장 새로 여는 함수
 	def __newFile(self):
-		self.__root.title("Untitled - Notepad")
+		self.__root.title("제목없음 - MemoX")
 		self.__file = None
 		self.__thisTextArea.delete(1.0,END)
 
+	# 메모장 저장하는 함수
 	def __saveFile(self):
-
 		if self.__file == None:
-			# Save as new file
-			self.__file = asksaveasfilename(initialfile='Untitled.txt',
-											defaultextension=".txt",
-											filetypes=[("All Files","*.*"),
-												("Text Documents","*.txt")])
+			self.__file = asksaveasfilename(initialfile='Untitled.txt', defaultextension=".txt", filetypes=[("All Files", "*.*"), ("Text Documents","*.txt")])
 
 			if self.__file == "":
 				self.__file = None
 			else:
-				# Try to save the file
 				file = open(self.__file,"Untitled")
 				file.write(self.__thisTextArea.get(1.0,END))
 				file.close()
-
-				# Change the window title
-				self.__root.title(os.path.basename(self.__file) + " - Notepad")
+				self.__root.title(os.path.basename(self.__file) + " - MemoX")
 
 		else:
 			file = open(self.__file,"w")
 			file.write(self.__thisTextArea.get(1.0,END))
 			file.close()
-
-	def __cut(self):
-		self.__thisTextArea.event_generate("<<Cut>>")
-
-	def __copy(self):
-		self.__thisTextArea.event_generate("<<Copy>>")
-
-	def __paste(self):
-		self.__thisTextArea.event_generate("<<Paste>>")
 
 	def run(self):
 		# 메인 애플리케이션 실행
